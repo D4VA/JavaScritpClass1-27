ab027988e2ce13393c9e1fc0d0c545f116cb30b8
Diego Velasquez <diegovelasquezavila@gmail.com>
2019-11-14 18:33:34 -0600

    Callback y manejo de errores

--- clase27.js
+++ clase27.js
@@ -1,1 +1,5 @@
+// javascript no puede ejecutar mas de 1 funcion a la vez
+// si tiene varias tareas, se ejecuta una por 1, no mas.
+//A esto se le llama asincronismo (investigar mas)
+
 //Como funciona el tiempo en javascript, ver clase27.html
