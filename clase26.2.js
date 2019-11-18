94b3757b35fd298e9a897d4aeca6460d63ca5d72
Diego Velasquez <diegovelasquezavila@gmail.com>
2019-11-18 08:36:08 -0600

    promiseJS

--- clase26.2.js
+++ clase26.2.js
@@ -3,16 +3,19 @@
 functionrecorredorDeListas(functionPasadaPorParametro){
 
     // Recorremos la lista.
-    // Utilizamos la palabra reservada this para referirnos al objeto que es dueño de este método.
+    // Utilizamos la palabra reservada this para referirnos 
+    //al objeto que es dueño de este método.
 
     for(var i=0; i<this.length; i++){
-        // Ejecutamos la función pasada como parámetro, la cual se le envía el elemento actual.
+        // Ejecutamos la función pasada como parámetro, la cual 
+        // se le envía el elemento actual.
         functionPasadaPorParametro(this[i]);
     }
 
 }
 
-// Creamos un arreglo con el nombre de varias personas y lo guardamos en una variable que llamaremos personas.
+// Creamos un arreglo con el nombre de varias personas y lo 
+// guardamos en una variable que llamaremos personas.
 personas = newArray('Martin','Enrique','Fernanda','Maria','Alejandro','Alondra');
 
 // Asignamos la función al método recorredor
--- clase31.js
+++ clase31.js
@@ -1,1 +1,37 @@
-new promise(responsive)
+const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
+const opts = { crossDomain: true }
+
+
+
+function obtenerPersonaje(id){
+    return new Promise((resolve, reject)=>{
+        const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`
+        $
+            .get(url, opts, function(data){
+                resolve(data)
+        })
+        .fail(() => rejec(id))
+    })
+}
+
+
+function onError(id){
+    console.log(`Sucedio un error al obtener el personaje ${id}`)
+}
+
+function imprimirNumero (){
+    return 
+}
+
+
+obtenerPersonaje(3)
+.then(function(personaje){
+    console.log(`El personaje 2 es`)
+})
+
+obtenerPersonaje(2)
+.then(function(personaje) {
+    console.log(`El personaje 2 es ${personaje.name}`)
+})
+.catch(onError)
